c84ba56f00b6a7678a4de83cc1534baa78a908ce
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-06-21 11:43:27 -0300

    test utils

---
+++ src/testUtils/Gallery.ts
@@ -0,0 +1,19 @@
+export default function getImages() {
+  return {
+    first: {
+      mobile: "/assets/product-zx9-speaker/mobile/image-gallery-1.jpg",
+      tablet: "/assets/product-zx9-speaker/tablet/image-gallery-1.jpg",
+      desktop: "/assets/product-zx9-speaker/desktop/image-gallery-1.jpg"
+    },
+    second: {
+      mobile: "/assets/product-zx9-speaker/mobile/image-gallery-2.jpg",
+      tablet: "/assets/product-zx9-speaker/tablet/image-gallery-2.jpg",
+      desktop: "/assets/product-zx9-speaker/desktop/image-gallery-2.jpg"
+    },
+    third: {
+      mobile: "/assets/product-zx9-speaker/mobile/image-gallery-3.jpg",
+      tablet: "/assets/product-zx9-speaker/tablet/image-gallery-3.jpg",
+      desktop: "/assets/product-zx9-speaker/desktop/image-gallery-3.jpg"
+    }
+  }
+}
