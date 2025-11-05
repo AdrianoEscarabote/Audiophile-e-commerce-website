d998c72f7c92deb933921a57e5a9fd57f3f4b521
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-11-05 12:18:23 -0300

    fix(storybook): streamline configuration in main.ts and manager.ts for clarity and consistency

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,15 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
+  framework: "@storybook/nextjs",
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-links",
     "@storybook/addon-interactions",
   ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-  staticDirs: ["../public"]
+  docs: { autodocs: "tag" },
+  staticDirs: ["../public"],
 };
 
 export default config;
